cc8a1d42a5b0a349d24ece1ca87ba5131c1f0326
DanielKrcek <dkrcek@gapps.zcu.cz>
2026-04-16 22:42:03 +0200

    test knihovny

---
+++ test_webrtc.py
@@ -0,0 +1,4 @@
+from unitree_webrtc_connect.webrtc_driver import UnitreeWebRTCConnection
+from unitree_webrtc_connect.constants import WebRTCConnectionMethod
+print("Import successful!")
+print("Verze:", UnitreeWebRTCConnection)
